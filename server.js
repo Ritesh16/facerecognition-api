09dd2a260760cf6b0e5c2a53706655277ffb2e87
Ritesh16 <ritesh.sharma.cse@gmail.com>
2020-08-25 22:16:50 -0400

    Adding code for singin/register/updating entries

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const db = knex({
     }
   });
 
-console.log(db.select('*').from('users'));
-
 const app = express();
 
 app.use(cors());
@@ -24,21 +22,91 @@ app.get('/', (req, res) => {
     res.status(200).json("Api is running");
 });
 
+// Get Profile
+app.get('/profile/:id', (req, res) => {
+  const {id} = req.params;
+
+  db.select('*').from('users')
+    .where({id})
+    .then(users => {
+        if(users.length){
+            res.json(users[0]);
+        }
+        else{
+            res.status(400).json('Not found');
+        }
+    })
+    .catch(error => res.json('Some error has occurred while fetching user.'));
+});
+
+// Update Enteries
+app.put('/image', (req,res) => {
+   const {id} = req.body; 
+   db('users').where('id', '=', id)
+     .increment('entries', 1)
+     .returning('entries')
+     .then(entries => {
+         res.json(entries[0])
+     })
+     .catch(error => res.status(400).json('Unable to get entries'))
+});
+
+// Register
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
-
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0])
+    const hash = bcrypt.hashSync(password);
+    
+    db.transaction(trx => {
+        trx.insert({
+            hash:hash,
+            email:email
+        })
+        .into('login')
+        .returning('email')
+        .then(userEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: userEmail[0],
+                    name: name,
+                    joined: new Date()
+                }).then(user => {
+                        res.json(user[0])
+                  })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
     .catch(err => {
-        res.status(400).json(err)
-    });
+        res.status(400).json('Some error has occured while registering')
+    })
+});
+
+// Sign in
+app.post('/signin', (req, res) => {
+
+    db.select('email', 'hash')
+      .from('login')
+      .where('email','=', req.body.email)
+      .then(data => {
+          if(data.length){
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            if(isValid){
+              return db.select('*').from('users')
+                  .where('email','=', req.body.email)
+                  .then(data => {
+                      res.json(data[0])
+                  })
+                  .catch(error=> res.status(400).json('User not found!'))
+            }
+            else{
+                res.status(400).json('Invalid credentials');
+            }  
+          }
+          else{
+              res.status(400).json('Invalid credentials');
+          }
+      });
 });
 
 app.listen(3000, ()=> {
